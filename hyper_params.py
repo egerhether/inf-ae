838f46f959f4e10f2ebde4fad0d5fa9ca9a6ed5e
Izabela Kurek <ikurek0309@gmail.com>
2025-06-10 14:23:17 +0200

    ml-10m hyperparams (for my local testing)

--- hyper_params.py
+++ hyper_params.py
@@ -39,6 +39,20 @@ hyper_params = {
         "user_support": -1,  # -1 implies use all users
         "seed": 42,
     },
+    "ml-10m": {
+        "dataset": "ml-10m",
+        "item_id": "item_id:token",  # configure it based on the .item file
+        "category_id": "type:token_seq",  # configure it based on the .item file
+        "use_gini": True,
+        "float64": False,
+        "depth": 1,
+        "grid_search_lamda": True,
+        "lamda": 1.0,  # Only used if grid_search_lamda == False
+        # Number of users to keep (randomly)
+        "user_support": 10000,  # -1 implies use all users
+        "seed": 42,
+        "batch_size": 20000
+    },
     "amazon_magazine": {
         "dataset": "amazon_magazine",
         "item_id": "item_id:token",  # configure it based on the .item file
